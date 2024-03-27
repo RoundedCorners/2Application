731945b50d0452ad064d0c5631927cde75cb69f8
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-27 02:58:46 +0200

    squash!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/core";
+import { createSignal } from "solid-js";
 import "./App.css";
+import logo from "./assets/logo.svg";
 
 function App() {
 	const [greetMsg, setGreetMsg] = createSignal("");
@@ -17,13 +17,13 @@ function App() {
 			<h1>Welcome to Tauri!</h1>
 
 			<div class="row">
-				<a href="https://vitejs.dev" target="_blank">
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
 					<img src="/vite.svg" class="logo vite" alt="Vite logo" />
 				</a>
-				<a href="https://tauri.app" target="_blank">
+				<a href="https://tauri.app" target="_blank" rel="noreferrer">
 					<img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
 				</a>
-				<a href="https://solidjs.com" target="_blank">
+				<a href="https://solidjs.com" target="_blank" rel="noreferrer">
 					<img src={logo} class="logo solid" alt="Solid logo" />
 				</a>
 			</div>
